01f034be5e8cb61a30a8b98dc66b803fbe36300b
Caio Castro <81255399+CaioCastro00@users.noreply.github.com>
2021-06-14 17:01:12 -0300

    Update utilidades.py

--- app/usuarios/utilidades.py
+++ app/usuarios/utilidades.py
@@ -5,19 +5,6 @@ from flask import flash, redirect, url_for, abort
 from flask_login import current_user
 from app import app, mail
 import os, secrets
-"""
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
-
-gauth = GoogleAuth()           
-drive = GoogleDrive(gauth)
-
-upload_file_list = ['1.jpg', '2.jpg']
-for upload_file in upload_file_list:
-	gfile = drive.CreateFile({'parents': [{'id': '1pzschX3uMbxU0lB5WZ6IlEEeAUE8MZ-t'}]})
-	# Read file and set it as the content of this instance.
-	gfile.SetContentFile(upload_file)
-	gfile.Upload() # Upload the file."""
 
 def gerar_token(email):
     serializer = URLSafeTimedSerializer(app.config['SECRET_KEY'])
